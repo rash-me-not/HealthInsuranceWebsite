561bbcf8e2fab3188e434f46232aafc538e3b7bb
Rashmita Mishra <42665636+rash-me-not@users.noreply.github.com>
2018-08-24 00:27:31 -0700

    Fixed css and images path

--- loginwelcome.php
+++ loginwelcome.php
@@ -1,3 +1,30 @@
+<<<<<<< HEAD
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./css/headerstyle.css">
+        <title>Features and Benefits - HEALTH INSURANCE</title>
+
+    </head>
+    <body>
+        <div class="header">
+            <a href="#default" class="logo">YOUR HEALTH IS AN INVESTMENT NOT AN EXPENSE</a>
+            <div class="header-right">
+                <a class="active" href="home.html">Home</a>
+                <a class="active" href="features.html">Features and Benefits</a>
+                <a class="active" href="registration.php">Registration</a>
+                <a class="active" href="login.php">Login</a>
+                <a class="active" href="contact.html">Contact</a>
+            </div>
+        </div>
+<?php
+session_start();
+?>
+<div class="body-content">
+<div class="alert alert-success"><?= $_SESSION['message'] ?></div>
+<div class="fw-title">Welcome <span class="user"><?= $_SESSION['username'] ?></span></div> <br>
+<div class="grid-item-body"><img src="./images/common-health-insurance-terms.png"/></div>
+</div>
+=======
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="headerstyle.css">
@@ -23,3 +50,4 @@ session_start();
 <div class="fw-title">Welcome <span class="user"><?= $_SESSION['username'] ?></span></div> <br>
 <div class="grid-item-body"><img src="common-health-insurance-terms.png"/></div>
 </div>
+>>>>>>> 91cdb653f27d92285992065ef2ad6151c361039b
